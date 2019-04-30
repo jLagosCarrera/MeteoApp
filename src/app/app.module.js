9703646b3d31a038ab8dffa569e15bed7f412e35
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-04-30 14:31:52 +0200

    feat(Task-2): Add AngularJS Material and mdbutton

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,4 +1,7 @@
 import angular from 'angular';
+import angularmd from 'angular-material';
+import angularanimate from 'angular-animate';
+import angulararia from 'angular-aria';
 import appComponent from './app.component';
 
-angular.module('app', []).component('app', appComponent);
+angular.module('app', [angularmd, angularanimate, angulararia]).component('app', appComponent);
