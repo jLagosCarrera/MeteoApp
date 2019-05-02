701fb2b55380cdd014347bcb14d092e844f44dbb
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-02 18:08:00 +0200

    refactor(Task-3): Created folder for component named component

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -5,7 +5,7 @@ import angularmd from 'angular-material';
 import angularanimate from 'angular-animate';
 import angulararia from 'angular-aria';
 import angularmessages from 'angular-messages';
-import appComponent from './app.component';
+import appComponent from './component/component';
 import helloComponent from './hello/hello';
 import worldComponent from './world/world';
 
---
+++ src/app/component/component.js
@@ -0,0 +1,8 @@
+import template from './component.html';
+import './component.scss';
+
+const AppComponent = {
+    template
+};
+
+export default AppComponent;
