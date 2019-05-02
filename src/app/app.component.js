81bb148b0bfa1c625973a536be71d7a6d058e809
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-02 12:57:15 +0200

    feat(Task-4): Create service for handling OWM request and use it on the component

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,8 +1,23 @@
 import template from './app.component.html';
 import './app.component.css';
 
+AppComponentController.$inject = ['$scope', 'weatherService'];
+
+function AppComponentController($scope, weatherService) {
+    $scope.logFetch = () => {
+        weatherService
+            .then(function (response) {
+                console.log(response.data);
+            })
+            .catch(function (response) {
+                console.log(response.data);
+            });
+    }
+}
+
 const AppComponent = {
-    template
+    template: template,
+    controller: AppComponentController
 };
 
 export default AppComponent;
--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,4 +1,23 @@
 import angular from 'angular';
 import appComponent from './app.component';
 
-angular.module('app', []).component('app', appComponent);
+const app = angular.module('app', []);
+
+app.component('app', appComponent);
+app.factory('weatherService', ['$http', function ($http) {
+    const URL = 'http://api.openweathermap.org/data/2.5/weather';
+
+    const request = {
+        method: 'GET',
+        url: URL,
+        params: {
+            q: 'Vigo',
+            mode: 'json',
+            units: 'metric',
+            lang: 'en',
+            appid: '37499dd44886aa89565abd094ecbfc8c'
+        }
+    };
+
+    return $http(request);
+}]);
