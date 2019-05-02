192be02ece6b99d1822fba95e1a9e491cb9f1934
Julio Lagos <46754911+jLagosCarrera@users.noreply.github.com>
2019-05-02 16:26:56 +0200

    feat(Task-1): As a developer, I want a solid start point for the Weather Project (#1)
    * feat(Task-1): Configure Webpack to our workflow
    
    * feat(Task-1): Add AngularJS hello world
    
    * feat(Task-1): Split config files for development and production
    
    * feat(Task-1): Add HtmlWebpackPlugin to serve index file
    
    * feat(Task-1): Add dist folder to gitignore

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+    entry: './src/app/app.module.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "/dist/"),
+        compress: true,
+        port: 9000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: path.resolve(__dirname, '../src/index.html'),
+            filename: path.resolve(__dirname, '../dist/index.html')
+        })
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: [':data-src']
+                    }
+                }
+            }
+        ]
+    }
+}
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+    entry: './src/app/app.module.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: path.resolve(__dirname, '../src/index.html'),
+            filename: path.resolve(__dirname, '../dist/index.html')
+        })
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: [':data-src']
+                    }
+                }
+            }
+        ]
+    }
+}
