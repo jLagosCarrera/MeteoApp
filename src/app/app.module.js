1844b9f6c0f21db245131185422ba5b8aaa27af3
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-02 11:31:02 +0200

    feat(Task-3): Add UI-Router and configured two components to switch with it

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,8 +1,26 @@
 import angular from 'angular';
+import uiRouter from '@uirouter/angularjs';
 import helloComponent from './hello/hello';
 import worldComponent from './world/world';
 
-const app = angular.module('app', []);
+const app = angular.module('app', [uiRouter]);
 
 app.component('hello', helloComponent);
 app.component('world', worldComponent);
+
+app.config(['$stateProvider', function (provider) {
+    var helloState = {
+        name: 'hello',
+        url: '/hello',
+        component: 'hello'
+    }
+
+    var constState = {
+        name: 'world',
+        url: '/world',
+        component: 'world'
+    }
+
+    provider.state(helloState);
+    provider.state(constState);
+}]);
