efe5bde1c1426bbaa78271a921943cf07b9d4304
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-03 12:19:46 +0200

    Refactor(Task-5): Clarified a few lines

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,11 +1,11 @@
-//Modules
+//Angular and Angularmd
 import angular from 'angular';
-import uiRouter from '@uirouter/angularjs';
 import angularmd from 'angular-material';
 import angularanimate from 'angular-animate';
 import angulararia from 'angular-aria';
 import angularmessages from 'angular-messages';
-//Routes
+//UiRouter and routes
+import uiRouter from '@uirouter/angularjs';
 import routes from './app.routes';
 //Services
 import actualWeather from './shared/services/actualWeather';
