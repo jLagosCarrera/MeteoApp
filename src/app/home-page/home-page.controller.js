522325bfde218b3908be1cb45678df4fee550b39
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-06 15:17:15 +0200

    feat(Task-7): Created list for recent searches and managed save and load it on localStorage

--- src/app/home-page/home-page.controller.js
+++ src/app/home-page/home-page.controller.js
@@ -1,9 +1,21 @@
 export default class HomePageController {
-    constructor() {
+    constructor($window) {
+        this.latestSearches = JSON.parse($window.localStorage.getItem('latestSearches')) || [];
         this.search = () => {
+            let latestSearches = this.latestSearches;
+            //Checks input is filled correctly
             if (this.city == undefined || this.city == null || this.city.trim() === '') {
                 return;
             }
+
+            latestSearches.unshift(this.city);
+            if (latestSearches.length > 10) {
+                latestSearches.splice(10, latestSearches.length - 10);
+            }
+
+            $window.localStorage.setItem('latestSearches', JSON.stringify(latestSearches));
         };
     }
 };
+
+HomePageController.$inject = ['$window']; 
