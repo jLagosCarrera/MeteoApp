d240faff0289ef27c1605a5f4800446537f6c8cc
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-15 16:01:05 +0200

    feat(Task-9): Change binding name to be different from component name

--- src/app/search-page/searchPage.controller.js
+++ src/app/search-page/searchPage.controller.js
@@ -17,7 +17,7 @@ export default class SearchPageController {
         this.geoNamesService.getNearbyCities(this.cityParam)
             .then((data) => {
                 const cities = data.map(city => city.name);
-                this.$timeout(() => this.nearbyCities = cities);
+                this.nearbyCities = cities;
             })
             .catch((error) => {
                 console.log(error); //TODO on next tickets
--- src/app/shared/components/displayable-list/displayableList.bindings.js
+++ src/app/shared/components/displayable-list/displayableList.bindings.js
@@ -1,5 +1,5 @@
 export default {
-    displayableList: '<',
+    listToDisplay: '<',
     displayIcon: '@',
     useHeader: '<'
 }
