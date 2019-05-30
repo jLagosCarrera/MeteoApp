af725725e6df932663a9b4fc765bdde256c56e5d
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-30 15:56:41 +0200

    fix(Task-20): Removed console log and made API call with the current language

--- src/app/shared/filters/capitalize.js
+++ src/app/shared/filters/capitalize.js
@@ -1,7 +1,6 @@
 export default (input, all) => {
     const REGEX_ALL = /([^\W_]+[^\s-]*) */g;
     const REGEX_FIRST = /([^\W_]+[^\s-]*)/;
-    console.log(input)
 
     if (!input) return '';
     const regex = (all) ? REGEX_ALL : REGEX_FIRST;
