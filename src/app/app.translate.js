6b522f01215604da3fecd183aedf37841686c7d3
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-30 15:10:24 +0200

    feat(Task-20): Add spanish and galician translations to the app

--- src/app/app.translate.js
+++ src/app/app.translate.js
@@ -1,9 +1,11 @@
 import en from '../resources/translations/en';
 import es from '../resources/translations/es';
+import gl from '../resources/translations/gl';
 
 export default ['$translateProvider', function ($translateProvider) {
     $translateProvider.translations('en', en);
     $translateProvider.translations('es', es);
+    $translateProvider.translations('gl', gl);
 
-    $translateProvider.preferredLanguage('en');
+    $translateProvider.preferredLanguage('es');
 }]
--- src/resources/translations/es.js
+++ src/resources/translations/es.js
@@ -1,5 +1,82 @@
 export default {
-    "TOOLTIPS": {
-        "OPENCONTACT": "Open contact form"
+    "CITY_SEARCH": {
+        "TOOLTIPS": {
+            "INSERT_CITY": "Introduce una ciudad aquí",
+            "SEARCH_CITY": "Busca tu ciudad!"
+        }
+    },
+    "CONTACT_FORM": {
+        "BUTTON_CANCEL": "Cancelar",
+        "BUTTON_SEND": "Enviar",
+        "INPUT_EMAIL": "Tu email",
+        "INPUT_ISSUE": "Problema",
+        "INPUT_MESSAGE": "Mensaje",
+        "INPUT_NAME": "Tu nombre",
+        "TITLE": "Contáctanos!",
+        "TOOLTIPS": {
+            "EXIT": "Salir",
+            "INSERT_EMAIL": "Introduce tu email aquí",
+            "INSERT_ISSUE": "Introduce tu problema aquí",
+            "INSERT_MESSAGE": "Explícanos tu problema aquí",
+            "INSERT_NAME": "Introduce tu nombre aquí",
+            "SEND_ISSUE": "Envia el problema"
+        }
+    },
+    "DISPLAYABLE_LIST": {
+        "TOOLTIPS": {
+            "HEADER": "lista de ciudades {{ type }}s",
+            "NO_RESULTS": "No hay resultados",
+            "SINGLE_CARD": "Ciudad {{ type }}"
+        },
+        "TYPES": {
+            "NEARBY": "cercana",
+            "RECENT": "reciente"
+        }
+    },
+    "ERROR_INFORM": {
+        "GO_HOME_BUTTON": "Ir al inicio",
+        "NOTHING_TO_SHOW": "Nada que ver aquí",
+        "RELOAD_BUTTON": "Recargar?",
+        "TOOLTIPS": {
+            "ERROR": "Código de error",
+            "GO_HOME": "Volver a la página de inicio",
+            "RELOAD": "Recargar la búsqueda de la ciudad"
+        },
+        "WORKING_ON_IT": "Lo sentimos! Trabajamos en ello..."
+    },
+    "FORECAST_CARD": {
+        "SUNRISE": "Amanecer",
+        "SUNSET": "Anochecer",
+        "TOOLTIPS": {
+            "AIR_PRESSURE": "Presión del aire en hPa",
+            "DATE": "Fecha de la previsión",
+            "PLACE_INFO": "Ciudad (País)",
+            "SUNRISE_INFO": "Hora local del amanecer",
+            "SUNSET_INFO": "Hora local del anochecer",
+            "TEMPERATURE": "Temperatura",
+            "TIME": "Hora local",
+            "WIND_DIRECTION": "Dirección del viento en grados",
+            "WIND_SPEED": "Velocidad del viento en km/h"
+        }
+    },
+    "PROGRESS_BAR": {
+        "TOOLTIPS": {
+            "BAR_INFO": "Porcentaje de {{ type }}"
+        },
+        "TYPES": {
+            "CLOUDS": "nubes",
+            "HUMIDITY": "humedad"
+        }
+    },
+    "SEARCH_PAGE": {
+        "CONTACT_BUTTON": "Necesitas ayuda?",
+        "TOOLTIPS": {
+            "OPEN_CONTACT": "Abrir formulario de contacto"
+        }
+    },
+    "WEBLOGO": {
+        "TOOLTIPS": {
+            "INFO": "Volver a la página de inicio"
+        }
     }
 }
