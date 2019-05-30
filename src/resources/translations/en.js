9f292ada07f36a7c82b63193165218f7fffc01b7
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-05-30 14:56:35 +0200

    feat(Task-20): Finished english translation file and changed hardcoded strings for values on the translations file

--- src/resources/translations/en.js
+++ src/resources/translations/en.js
@@ -1,22 +1,4 @@
 export default {
-    "SEARCH_PAGE": {
-        "CONTACT_BUTTON": "Need help?",
-        "TOOLTIPS": {
-            "OPEN_CONTACT": "Open contact form",
-        },
-    },
-
-    "DISPLAYABLE_LIST": {
-        "TYPES": {
-            "RECENT": "Recent",
-            "NEARBY": "Nearby"
-        },
-
-        "TOOLTIPS": {
-            "INSERT_CITY": "Insert a city here",
-        }
-    },
-
     "CITY_SEARCH": {
         "TOOLTIPS": {
             "INSERT_CITY": "Insert a city here",
@@ -27,7 +9,7 @@ export default {
     "CONTACT_FORM": {
         "TITLE": "Contact us!",
         "INPUT_NAME": "Your name",
-        "INPUT_EMAIL": "Your eMail",
+        "INPUT_EMAIL": "Your email",
         "INPUT_ISSUE": "Issue",
         "INPUT_MESSAGE": "Message",
         "BUTTON_CANCEL": "Cancel",
@@ -38,7 +20,70 @@ export default {
             "INSERT_ISSUE": "Insert the issue you had here",
             "INSERT_MESSAGE": "Explain us your problem here",
             "SEND_ISSUE": "Send the issue",
-            "EXIT": "Exit",
+            "EXIT": "Exit"
+        }
+    },
+
+    "DISPLAYABLE_LIST": {
+        "TYPES": {
+            "RECENT": "Recent",
+            "NEARBY": "Nearby"
+        },
+        "TOOLTIPS": {
+            "HEADER": "{{ type }} cities list",
+            "SINGLE_CARD": "{{ type }} city",
+            "NO_RESULTS": "No results"
+        }
+    },
+
+    "ERROR_INFORM": {
+        "NOTHING_TO_SHOW": "Nothing here to show",
+        "WORKING_ON_IT": "Sorry! We're working on it...",
+        "RELOAD_BUTTON": "Try reload?",
+        "GO_HOME_BUTTON": "Go home",
+        "TOOLTIPS": {
+            "ERROR": "Error code",
+            "RELOAD": "Reload city forecast",
+            "GO_HOME": "Go back to home page"
+        }
+    },
+
+    "FORECAST_CARD": {
+        "SUNRISE": "Sunrise",
+        "SUNSET": "Sunset",
+        "TOOLTIPS": {
+            "SUNRISE_INFO": "Sunrise local time",
+            "SUNSET_INFO": "Sunset local time",
+            "PLACE_INFO": "City (Country)",
+            "DATE": "Date of the forecast",
+            "TIME": "Local time",
+            "TEMPERATURE": "Temperature",
+            "WIND_DIRECTION": "Wind direction on degrees",
+            "WIND_SPEED": "Wind speed on km/h",
+            "AIR_PRESSURE": "Air pressure on hPa"
+        }
+    },
+
+    "PROGRESS_BAR": {
+        "TYPES": {
+            "CLOUDS": "clouds",
+            "HUMIDITY": "humidity"
+        },
+        "TOOLTIPS": {
+            "BAR_INFO": "Percentage of {{ type }}",
+        }
+    },
+
+    "SEARCH_PAGE": {
+        "CONTACT_BUTTON": "Need help?",
+        "TOOLTIPS": {
+            "OPEN_CONTACT": "Open contact form"
+        }
+    },
+
+    "WEBLOGO": {
+        "TOOLTIPS": {
+            "INFO": "Go back to homepage",
         }
     }
 }
