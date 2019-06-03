a5da9b6442a472ed864793dc295015f49f3b5734
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-06-03 16:46:31 +0200

    fix(Task-18): Converted two functions to be arrow functions

--- src/app/app.title.js
+++ src/app/app.title.js
@@ -3,12 +3,12 @@ export default ['$transitions', '$window', '$q', ($transitions, $window, $q) => 
         let title = '';
         const tokens = transition.getResolveTokens();
         const resolves = {};
-        const promises = tokens.map(function (token) {
+        const promises = tokens.map((token) => {
             const resolved = transition.injector().get(token);
             resolves[token] = resolved;
             return resolved;
         });
-        $q.all(promises).then(function () {
+        $q.all(promises).then(() => {
             title = resolves.$title;
             $window.document.title = !title ? 'MeteoApp' : title;
         })
