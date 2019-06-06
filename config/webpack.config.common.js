b193f21367fcc3db43d99b66302bd20a3ae258df
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-06-06 15:10:14 +0200

    feat(Task-25): Imported babel to the project

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -36,6 +36,16 @@ module.exports = {
             {
                 test: /\.(png|jpg|svg)$/,
                 use: ['url-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     }
