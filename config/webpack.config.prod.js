caaf6972e7d35842540d9b5013fce24a18a37f5e
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-06-06 14:53:04 +0200

    feat(Task-24): Created common config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,49 +1,13 @@
 const CopyPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-const path = require('path');
+const webpackMerge = require('webpack-merge');
+const common = require('./webpack.config.common');
 
-module.exports = {
+module.exports = webpackMerge(common, {
     mode: 'production',
-    entry: './src/app/app.module.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, '../dist')
-    },
     plugins: [
-        new HtmlWebpackPlugin({
-            hash: true,
-            template: './src/index.html',
-            filename: './index.html'
-        }),
-        new FaviconsWebpackPlugin('./src/resources/img/favicon.png'),
-        new Dotenv({
-            systemvars: true,
-        }),
         new CopyPlugin([{
-                from: './src/resources/translations',
-                to: './resources/translations'
-            },
-        ]),
-    ],
-    module: {
-        rules: [{
-                test: /\.(html)$/,
-                use: ['html-loader']
-            },
-            {
-                test: /\.(s*)css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: ['url-loader']
-            },
-            {
-                test: /\.(png|jpg|svg)$/,
-                use: ['url-loader']
-            }
-        ]
-    }
-}
+            from: './src/resources/translations',
+            to: './resources/translations'
+        }])
+    ]
+});
