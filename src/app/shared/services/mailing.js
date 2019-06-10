c727e05473c4eab321e6c6aca0a4e6d19baa3258
Julio Lagos <julio.lagos.carrera@gmail.com>
2019-06-10 14:19:49 +0200

    feat(Task-28): Documented 2 services

--- src/app/shared/services/mailing.js
+++ src/app/shared/services/mailing.js
@@ -1,12 +1,30 @@
 import * as emailjs from 'emailjs-com';
 
+/**
+ * EmailJS API service functions.
+ *
+ * @class Mailing
+ */
 export default class Mailing {
+    /**
+     * Creates an instance of Mailing and injects needed dependencies.
+     * 
+     * @param {*} $mdToast - Toast service from AngularJS Material
+     * @param {*} $translate - Translate service from angular-translate
+     * @memberof Mailing
+     */
     constructor($mdToast, $translate) {
         this.$mdToast = $mdToast;
         this.$translate = $translate;
         emailjs.init(process.env.EMAILJS_API_KEY);
     }
 
+    /**
+     * Sends mail with object data through emailJS API.
+     *
+     * @param {Object} answer - Data being sended through email
+     * @memberof Mailing
+     */
     contact(answer) {
         emailjs.send(process.env.EMAILJS_SERVICE, process.env.EMAILJS_TEMPLATE, answer)
             .then((response) => {
@@ -16,6 +34,13 @@ export default class Mailing {
             });
     }
 
+    /**
+     * Shows an AngularJS Material toast.
+     *
+     * @param {string} theme - Theme used for the toast
+     * @param {string} translate - Translation message displayed on the toast
+     * @memberof Mailing
+     */
     showToast(theme, translate) {
         this.$mdToast.show(
             this.$mdToast.simple()
